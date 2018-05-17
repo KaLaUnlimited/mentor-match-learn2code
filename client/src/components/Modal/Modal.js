a3c05d45442a8faba6bd3e461939124a60ce0a4d
Esterling Accime <esterling@brainbytescreative.com>
2018-05-16 22:43:25 -0400

    fix: Fix modal issues

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,56 +1,46 @@
-import React from 'react';
-import { Button, Modal, OverlayTrigger, Popover, Tooltip } from 'react-bootstrap';
+import React from "react";
+import {
+  Button,
+  Modal,
+  OverlayTrigger,
+  Popover,
+  Tooltip
+} from "react-bootstrap";
 import "./Modal.css";
-import Form from "../Form/Form.js"
+import Form from "../Form/Form.js";
 
-export default class Interests extends React.Component {
-  constructor(props, context) {
-    super(props, context);
+const Interests = props => {
+  const popover = (
+    <Popover id="modal-popover" title="popover">
+      very popover. such engagement
+    </Popover>
+  );
 
-    this.handleShow = this.handleShow.bind(this);
-    this.handleClose = this.handleClose.bind(this);
+  const tooltip = <Tooltip id="modal-tooltip">wow.</Tooltip>;
 
-    this.state = {
-      show: false
-    };
-  }
+  return (
+    <div>
+      <Button
+        className="tech-btn pull-right"
+        bsSize="medium"
+        onClick={props.handleShow}
+      >
+        Choose your Path
+      </Button>
 
-  handleClose() {
-    this.setState({ show: false });
-  }
+      <Modal className="modal" show={props.show} onHide={props.handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Choose your Path</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form {...props} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.handleClose}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
 
-  handleShow() {
-    this.setState({ show: true });
-  }
-
-  render() {
-    const popover = (
-      <Popover id="modal-popover" title="popover">
-        very popover. such engagement
-      </Popover>
-    );
-    const tooltip = <Tooltip id="modal-tooltip">wow.</Tooltip>;
-
-    return (
-      <div>
-        <Button className="tech-btn pull-right" bsSize="medium" onClick={this.handleShow}>
-          Choose your Path
-        </Button>
-
-        <Modal className="modal" show={this.state.show} onHide={this.handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Choose your Path</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-          <Form auth={this.props.auth}/>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.handleClose}>Close</Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
-    );
-  }
-}
-
-
+export default Interests;
